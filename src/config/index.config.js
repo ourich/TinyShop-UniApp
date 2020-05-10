a8056a04b8c5b16d9ddd03f07bdd56f043a73a34
richat <richat@126.com>
2020-05-10 11:39:31 +0800

    正式环境

--- src/config/index.config.js
+++ src/config/index.config.js
@@ -11,8 +11,8 @@ const CONFIG = {
     //生产环境配置
     production: {
         assetsPath: '/static', // 静态资源路径
-        baseUrl: 'http://demo.rageframe.com/api',  // 后台接口请求地址
-        hostUrl: 'http://h5.tinyshop.rageframe.com',        // H5地址(前端运行地址)
+        baseUrl: 'http://open.wecshop.com/api',  // 后台接口请求地址
+        hostUrl: 'http://h5.wecshop.com',        // H5地址(前端运行地址)
         websocketUrl: '',        // websocket服务端地址
         weixinAppId: '' // 微信公众号appid
     }
