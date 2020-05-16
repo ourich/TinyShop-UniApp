c3d91ee66e25afaaa15f1ea7d24733ec7f2fbdbb
richat <richat@126.com>
2020-05-16 21:50:55 +0800

    生成H5

--- src/config/index.config.js
+++ src/config/index.config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     development: {
         assetsPath: '/static', // 静态资源路径
         baseUrl: 'http://rf.com/api',  // 后台接口请求地址
-		baseUrl: 'http://open.wecshop.com/api',  // 后台接口请求地址
+		// baseUrl: 'http://open.wecshop.com/api',  // 后台接口请求地址
         hostUrl: 'http://localhost:8080',        // H5地址(前端运行地址)
         websocketUrl: '',        // websocket服务端地址
         weixinAppId: '' // 微信公众号appid
@@ -11,8 +11,8 @@ const CONFIG = {
     //生产环境配置
     production: {
         assetsPath: '/static', // 静态资源路径
-        baseUrl: 'http://open.wecshop.com/api',  // 后台接口请求地址
-        hostUrl: 'http://h5.wecshop.com',        // H5地址(前端运行地址)
+        baseUrl: 'http://open.jiangsuwxw.com/api',  // 后台接口请求地址
+        hostUrl: 'http://h5.jiangsuwxw.com/',        // H5地址(前端运行地址)
         websocketUrl: '',        // websocket服务端地址
         weixinAppId: '' // 微信公众号appid
     }
