64a1df20833969d6576c082b3c05a68b18c97737
richat <richat@126.com>
2020-05-31 11:43:41 +0800

    新的本地测试地址

--- src/config/index.config.js
+++ src/config/index.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     //开发环境配置
     development: {
         assetsPath: '/static', // 静态资源路径
-		baseUrl: 'http://rf.com/api',  // 后台接口请求地址
+		baseUrl: 'http://ra.com/api',  // 后台接口请求地址
         // baseUrl: 'http://192.168.1.7/api',  // 后台接口请求地址
 		// baseUrl: 'https://open.jiangsuwxw.com/api',  // 后台接口请求地址
         hostUrl: 'http://localhost:8080',        // H5地址(前端运行地址)
