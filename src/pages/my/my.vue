057d65368545aad78f12f171ed81417176aa0bd6
richat <richat@126.com>
2020-06-06 10:08:43 +0800

    新版会员中心

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -22,10 +22,20 @@
 		<!--header-->
 		<view class="tui-mybg-box">
 			<image :src="webURL+'/static/images/mall/my/img_bg_3x.png'" class="tui-my-bg" mode="widthFix"></image>
-			<view class="tui-header-center">
-				<image src="/static/images/my/mine_def_touxiang_3x.png" class="tui-avatar" @tap="href(3)"></image>
+			<view class="tui-header-center" @tap="navTo(userInfo ? '/pages/user/userinfo/userinfo' : 'login')">
+				<image src="/static/images/mall/my/mine_def_touxiang_3x.png" class="tui-avatar" @tap="href(3)"></image>
 				<view class="tui-info">
-					<view class="tui-nickname">呼噜猪zzZ <image src="/static/images/mall/my/icon_vip_3x.png" class="tui-img-vip"></image>
+					<view class="tui-nickname">
+						<text class="username" v-if="hasLogin">
+							{{
+								userInfo.nickname ||
+								userInfo.realname ||
+								userInfo.mobile ||
+								'暂无昵称'
+							}}
+						</text>
+						<text class="username" v-else>登录/注册</text>
+						<image src="/static/images/mall/my/icon_vip_3x.png" class="tui-img-vip"></image>
 					</view>
 					<view class="tui-explain">这家伙很懒…</view>
 				</view>
@@ -291,77 +301,6 @@
 				top: 0, //标题图标距离顶部距离
 				opcity: 0,
 				scrollTop: 0.5,
-				productList: [{
-						img: 1,
-						name: "欧莱雅（LOREAL）奇焕光彩粉嫩透亮修颜霜 30ml（欧莱雅彩妆 BB霜 粉BB 遮瑕疵 隔离）",
-						sale: 599,
-						factory: 899,
-						payNum: 2342
-					},
-					{
-						img: 2,
-						name: "德国DMK进口牛奶  欧德堡（Oldenburger）超高温处理全脂纯牛奶1L*12盒",
-						sale: 29,
-						factory: 69,
-						payNum: 999
-					},
-					{
-						img: 3,
-						name: "【第2支1元】柔色尽情丝柔口红唇膏女士不易掉色保湿滋润防水 珊瑚红",
-						sale: 299,
-						factory: 699,
-						payNum: 666
-					},
-					{
-						img: 4,
-						name: "百雀羚套装女补水保湿护肤品",
-						sale: 1599,
-						factory: 2899,
-						payNum: 236
-					},
-					{
-						img: 5,
-						name: "百草味 肉干肉脯 休闲零食 靖江精制猪肉脯200g/袋",
-						sale: 599,
-						factory: 899,
-						payNum: 2399
-					},
-					{
-						img: 6,
-						name: "短袖睡衣女夏季薄款休闲家居服短裤套装女可爱韩版清新学生两件套 短袖粉色长颈鹿 M码75-95斤",
-						sale: 599,
-						factory: 899,
-						payNum: 2399
-					},
-					{
-						img: 1,
-						name: "欧莱雅（LOREAL）奇焕光彩粉嫩透亮修颜霜",
-						sale: 599,
-						factory: 899,
-						payNum: 2342
-					},
-					{
-						img: 2,
-						name: "德国DMK进口牛奶",
-						sale: 29,
-						factory: 69,
-						payNum: 999
-					},
-					{
-						img: 3,
-						name: "【第2支1元】柔色尽情丝柔口红唇膏女士不易掉色保湿滋润防水 珊瑚红",
-						sale: 299,
-						factory: 699,
-						payNum: 666
-					},
-					{
-						img: 4,
-						name: "百雀羚套装女补水保湿护肤品",
-						sale: 1599,
-						factory: 2899,
-						payNum: 236
-					}
-				],
 				pageIndex: 1,
 				loadding: false,
 				pullUpOn: true
